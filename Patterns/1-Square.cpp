ba56cf57d706a158dda5d9723c3f6473e96e7967
prajjwalag <50009834+prajjwalag@users.noreply.github.com>
2023-02-01 21:57:55 +0530

    Create 1-Square.cpp

---
+++ Patterns/1-Square.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int size;
+  cout<<"Enter Size: ";
+  cin>>size;
+
+
+  for(int row = 0; row < size; row++) {
+    for(int col = 0; col < size; col++) {
+      cout << "* ";
+    }
+    cout<< endl;
+  }
+}
