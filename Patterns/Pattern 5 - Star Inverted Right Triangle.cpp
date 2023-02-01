e3108fe49e0be9d43d352d2f43b732cf21a96875
prajjwalag <50009834+prajjwalag@users.noreply.github.com>
2023-02-01 22:08:01 +0530

    Create Pattern 5 - Star Inverted Right Triangle.cpp

---
+++ Patterns/Pattern 5 - Star Inverted Right Triangle.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int size;
+  cout<<"Enter Size: ";
+  cin>>size;
+
+  for(int row = 0; row < size; row++) {
+    for(int col = 0; col < size-row; col++) {
+      cout << "* ";
+    }
+    cout<< endl;
+  }
+}
