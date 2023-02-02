ec82b7b0b80237a50ca580217fc6ecd3a426c793
prajjwalag <50009834+prajjwalag@users.noreply.github.com>
2023-02-02 23:01:52 +0530

    Create Pattern - 1.java

---
+++ Striver's DSA Sheet Patterns/Pattern - 1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    static void printPattern(int n) {
+        System.out.println();
+        for(int i=0; i<n; i++) {
+            for(int j = 0; j<n; j++) {
+                    System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+  
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size: ");
+        int n = sc.nextInt();
+
+        printPattern(n);
+    }
+}
